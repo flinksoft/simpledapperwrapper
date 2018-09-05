e592a78dba4cbf6ccfb579f81574974cddd32d67
flinksoft <flinksoft@protonmail.ch>
2018-09-05 21:44:39 +0100

    Initial check-in of module SimpleDapperWrapper

---
+++ SimpleDapperWrapper/Interfaces/IDapperContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SimpleDapperWrapper.Interfaces
+{
+    public interface IDapperContext : IDisposable
+    {
+        IDbConnection Connection { get; }
+        T Transaction<T>(Func<IDbTransaction, T> query);
+        IDbTransaction BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
---
+++ SimpleDapperWrapper/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using Dapper;
+using System.Collections.Generic;
+
+namespace SimpleDapperWrapper.Interfaces
+{
+    public interface ISession
+    {
+        IEnumerable<T> Query<T>(string query, object param = null);
+        void Execute(string sql, DynamicParameters parameters);
+    }
+}
